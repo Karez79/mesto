46e4367e7648f632d5120891a97ffc958dab3c9f
Ivan Kozlov <karez78@yandex.ru>
2023-03-05 20:43:49 +0100

    после первого ревью

--- README.md
+++ README.md
@@ -2,17 +2,14 @@
 
 ### Обзор
 
-* Figma
-* Картинки
+Это 4 спринт от ["Яндекс.Практикум"](https://practicum.yandex.ru/) на курсе ["Веб-разработчик"](https://practicum.yandex.ru/web/).
 
-**Figma**
+* Начали изучать JavaScript и работу с браузером;
+* Знакомство с DOM;
+* Дебаггинг JavaScript;
+* HTML и CSS.
+
 
 * [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
 * [Ссылка на сайт](https://karez79.github.io/mesto/)
 
-**Картинки**
-
-Доставать картинки предстоит из Фигмы. Это расхожая практика, поэтому полезно потренироваться.
-Не забудьте [оптимизировать картинки](https://tinypng.com/), чтобы ваш сайт загружался быстрее.
-
-Удачи!
--- scripts/scrips.js
+++ scripts/scrips.js
@@ -1,7 +1,7 @@
 let profileTitile = document.querySelector('.profile__title');
 let profileSubbtitle = document.querySelector('.profile__subtitle');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input_type_title') ;
+let nameInput = document.querySelector('.popup__input_type_title');
 let jobInput = document.querySelector('.popup__input_type_subtitle');
 let popup = document.querySelector('.popup');
 const openPopupButton = document.querySelector('.profile__edit-button');
@@ -11,11 +11,9 @@ const saveBtn = document.querySelector('.popup__save-button');
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileTitile.textContent = nameInput.value;
-  profileSubbtitle .textContent = jobInput.value;
-  popup.classList.remove('popup_opened');
+  profileSubbtitle.textContent = jobInput.value;
+  closePopup('popup_opened');
 }
-
-formElement.addEventListener('submit', handleFormSubmit);
 
 function openPopup() {
   popup.classList.add('popup_opened');
@@ -23,10 +21,14 @@ function openPopup() {
   jobInput.value = profileSubbtitle.textContent;
 }
 
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+
 openPopupButton.addEventListener('click', openPopup);
 
-function closePopup() {
-    popup.classList.remove('popup_opened');
-  }
-  
-  closePopupButton.addEventListener('click', closePopup);
+
+closePopupButton.addEventListener('click', closePopup);
