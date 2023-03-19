c753c7c2bf40c28cbb8acff60071670dec72fea9
Ivan Kozlov <karez78@yandex.ru>
2023-03-19 14:06:35 +0100

    5 спринт первая проверка

--- scripts/scrips.js
+++ scripts/scrips.js
@@ -1,4 +1,3 @@
-
 let profileTitile = document.querySelector('.profile__title');
 let profileSubbtitle = document.querySelector('.profile__subtitle');
 let popup = document.querySelector('.popup');
@@ -9,25 +8,30 @@ const nameInput = document.querySelector('.popup__input_type_title');
 const jobInput = document.querySelector('.popup__input_type_subtitle');
 const closePopupButton = document.querySelector('.popup__close-button');
 const popupEditProdileSave = document.querySelector('.popup__form');
-
-
+const CardButtonAdd = document.querySelector('.profile__add-button')
+const CardPopupAdd = document.querySelector('#cardADD');
+const CardPopupAddForm = document.querySelector('#cardform');
 const closeButtonAdd = document.querySelector('.popup__close-addbutton');
 const cardsContainer = document.querySelector('.elements');
+const element = document.querySelector('.cards');
+const popupBigFoto = document.querySelector('#zoomfoto');
+const popupBigFotoTitle = document.querySelector('.popup__title-zoom');
+const popupBigFotoImage = document.querySelector('.popup__zoom');
+const popupBigFotoCloseBtn = document.querySelector('.popup__close-zoombutton');
 
 
 //Popup редактирования и сохранения профиля//
 
 openPopupButton.addEventListener('click', () => {
-popup.classList.add('popup_opened');
-nameInput.value = profileTitile.textContent;
-jobInput.value = profileSubbtitle.textContent;
-popupProfileEdit.classList.add('popup_opened');
+  popup.classList.add('popup_opened');
+  nameInput.value = profileTitile.textContent;
+  jobInput.value = profileSubbtitle.textContent;
+  popupProfileEdit.classList.add('popup_opened');
 });
 
 
 closePopupButton.addEventListener('click', () => {
   popupProfileEdit.classList.remove('popup_opened');
- 
 });
 
 function handleFormSubmit(evt) {
@@ -37,49 +41,33 @@ function handleFormSubmit(evt) {
   popupProfileEdit.classList.remove('popup_opened');
 }
 
-popupEditProdileSave.addEventListener('submit', handleFormSubmit);
-
-
-
 //Popup добавления карточи//
-const CardButtonAdd = document.querySelector('.profile__add-button')
-const CardPopupAdd = document.querySelector('#cardADD');
-const CardPopupAddForm = document.querySelector('#cardform');
 
 CardButtonAdd.addEventListener('click', () => {
   CardPopupAdd.classList.add('popup_opened');
   document.querySelector('.popup__input_type_name').value = '';
   document.querySelector('.popup__input_type_linkimage').value = '';
-
 });
 
 closeButtonAdd.addEventListener('click', () => {
   CardPopupAdd.classList.remove('popup_opened');
-
 });
 
-function handleFormCardAddSubmit (evt) {
+function handleFormCardAddSubmit(evt) {
   evt.preventDefault();
   const name = document.querySelector('.popup__input_type_name').value;
   const link = document.querySelector('.popup__input_type_linkimage').value;
   const card = {
-name:name,
-link:link 
-}
-CreateCardManual(card);
+    name: name,
+    link: link
+  }
+  CreateCardManual(card);
+  countCards();
   CardPopupAdd.classList.remove('popup_opened');
-  
 }
-
-CardPopupAddForm.addEventListener('submit', handleFormCardAddSubmit)
-
-
-
 
 // //Фотографии//
-
-const initialCards = [
-  {
+const initialCards = [{
     name: 'Тихокеанский морж.',
     link: './images/tihmorj.jpeg',
     alt: 'Тихокеанский морж.'
@@ -97,7 +85,7 @@ const initialCards = [
   {
     name: 'Новозеландский морж.',
     link: './images/newzelandkot.jpeg',
-    alt: 'Новозеландский морж.'
+    alt: '>Новозеландский морж.'
   },
   {
     name: 'Капский морской кот.',
@@ -109,24 +97,21 @@ const initialCards = [
     link: './images/fernandkot.jpeg',
     alt: 'Фернандесский морской кот.'
   }
-]; 
+];
 
-const element = document.querySelector('.cards');
-
-
-
+// Создание/сохранение карт + лайки//
 
 const CreateCard = (fotocard) => {
   const newCard = document.querySelector('#cardTemplate').content.cloneNode(true);
-  const cardHeading = newCard.querySelector('.card__info-title')
-  cardHeading.textContent = fotocard.name
-  const cardImage = newCard.querySelector('.cards__image')
-  cardImage.setAttribute('src', fotocard.link)
-  cardImage.setAttribute('alt', fotocard.alt)
-  const deleteButtonCard = newCard.querySelector('.cards__delete-foto')
-  deleteButtonCard.addEventListener('click', handleDeleteButtonCklik)
-  const changeColor = newCard.querySelector('.card__info-heart')
-  changeColor.addEventListener('click', handleLikeImageCklik)
+  const cardHeading = newCard.querySelector('.card__info-title');
+  cardHeading.textContent = fotocard.name;
+  const cardImage = newCard.querySelector('.cards__image');
+  cardImage.setAttribute('src', fotocard.link);
+  cardImage.setAttribute('alt', fotocard.name);
+  const deleteButtonCard = newCard.querySelector('.cards__delete-foto');
+  deleteButtonCard.addEventListener('click', handleDeleteButtonCklik);
+  const changeColor = newCard.querySelector('.card__info-heart');
+  changeColor.addEventListener('click', handleLikeImageCklik);
 
   return newCard;
 }
@@ -139,33 +124,36 @@ const CreateCardAuto = (fotocard) => {
 }
 
 initialCards.forEach(CreateCardAuto);
+countCards();
 
-function  handleLikeImageCklik (event){
+function handleLikeImageCklik(event) {
   event.target.classList.toggle('card__info-heart_active');
 }
 
-function handleDeleteButtonCklik (event){
-
+function handleDeleteButtonCklik(event) {
   const DeleteCard = event.target;
-  const  card = DeleteCard.closest('.card');
+  const card = DeleteCard.closest('.card');
   card.remove();
 }
 
+function openZoomPhoto(photo) {
+  photo.addEventListener('click', e => {
+    popupBigFoto.classList.add('popup_opened');
+    popupBigFotoTitle.textContent = e.target.alt;
+    popupBigFotoImage.src = e.target.src;
+  })
+};
 
-const popupBigFoto = document.querySelector('#zoomfoto');
-const popupBigFotoCloseBtn= document.querySelector('.popup__close-zoombutton');
+function countCards() {
+  const openPopupZoomphotos = document.querySelectorAll('.cards__image');
+  openPopupZoomphotos.forEach(photo => {
+    openZoomPhoto(photo);
+  })
+};
 
 popupBigFotoCloseBtn.addEventListener('click', () => {
   popupBigFoto.classList.remove('popup_opened');
 })
+popupEditProdileSave.addEventListener('submit', handleFormSubmit);
 
-const openPopupZoomphotos = document.querySelectorAll('.cards__image');
-
-openPopupZoomphotos.forEach(photo => {
-  photo.addEventListener('click', e => {
-    console.log(e.target.src, e.target.alt);
-    popupBigFoto.classList.add('popup_opened');
-    popupBigFoto.querySelector('.popup__title-zoom').textContent = e.target.alt;
-    popupBigFoto.querySelector('.popup__zoom').src = e.target.src;
-  })
-})
+CardPopupAddForm.addEventListener('submit', handleFormCardAddSubmit);
